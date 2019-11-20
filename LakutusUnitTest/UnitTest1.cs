c6064288c9b58cf0282e973ecab1942e416a3a05
Mika <mika.morsky@gmail.com>
2019-11-21 00:48:50 +0200

    Added unit test project

---
+++ LakutusUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LakutusUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ LaskutusTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaskutusTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
